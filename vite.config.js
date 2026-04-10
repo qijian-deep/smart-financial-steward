6b7b4fb762995a41057ceb205a2e952cd8869038
qijian-deep <qijian-deep@users.noreply.github.com>
2026-04-10 23:27:45 +0800

    feat: 提交

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/smart-financial-steward/',
   server: {
     host: '0.0.0.0',  // 允许外部访问
     port: 5173,       // 默认端口
